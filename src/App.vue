f0779fe7dcd452032cb2c7af99bb7692f161143b
Jhon Álvarez <jwalvarez@uninorte.edu.co>
2024-09-25 13:31:26 -0500

    x

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const apiResponse = ref('');
+
+async function getData() {
+  axios.get('URL_HERE')
+    .then(response => {
+      apiResponse.value = response
+    })
+    .catch(error => {
+      apiResponse.value = error;
+      console.log(error)
+    })
+}
+
+onMounted(() => {
+  console.log('mounted')
+  getData()
+})
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    {{ apiResponse }}
+  </div>
+</template>
